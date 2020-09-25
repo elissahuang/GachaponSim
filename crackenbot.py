166c6d9c86b5f13581c4254e07e1c2a97252087a
Eric Hsieh <ehsieh@lawn-143-215-118-187.lawn.gatech.edu>
2020-09-25 19:55:39 -0400

    Implemented basic rolling

--- crackenbot.py
+++ crackenbot.py
@@ -1,5 +1,7 @@
 import os
 import discord
+import random
+
 from dotenv import load_dotenv
 from discord.ext import commands
 from collections import OrderedDict
@@ -72,8 +74,58 @@ async def preview(ctx, gacha_num: int):
 
         await ctx.send(s)
 
+@bot.command('roll', help = "Roll for a selected gacha")
+@commands.has_role('true mmo players')
+async def roll(ctx, gacha_num: int): 
+    await ctx.send('Fetching gacha...')
+    if not index:
+        f = open('Gachas/index.txt', 'r')
+        s = 'Here is the list of all currently available gacha.\n'
+        i = 1
+        for line in f:
+            c = line.strip().split(':') # month-year:name
+            index[c[1].strip()] = c[0]
+            fm = str(i) + ". " + c[0] + " " + c[1] + '\n'
+            s = s + fm
+            i += 1
+        f.close()
+    index_list = list(index.items())
+    if gacha_num > len(index_list) or gacha_num < 1:
+        await ctx.send('Gacha not found.')
+    else:
+        wanted = index_list[gacha_num - 1]
+        path = 'Gachas/' + str(wanted[1]) + '-Gacha.txt'
+        s = wanted[0] + ' ' + wanted[1] + '\n'
+        f = open(path)
+        next(f)
+        next(f)
 
+        roll_dict = {}
 
+        for line in f:
+            p, n = line.strip().split(':')
+            n = n.replace('_', ' ')
+            p = float(p) * 100
+            p = int(p)
+            s = s + str(p) + '% ' + n + '\n'
+            roll_dict[p] = n
+        f.close()
+
+        rand_roll = random.random() * 100
+        if rand_roll < 31:
+            roll_out = roll_dict[31]
+        elif rand_roll > 31 and rand_roll < 59:
+            roll_out = roll_dict[28]
+        elif rand_roll > 59 and rand_roll < 82:
+            roll_out = roll_dict[23]
+        elif rand_roll > 82 and rand_roll < 89:
+            roll_out = roll_dict[7]
+        elif rand_roll > 89 and rand_roll < 95:
+            roll_out = roll_dict[6]
+        elif rand_roll > 95:
+            roll_out = roll_dict[5]
+
+        await ctx.send(roll_out)
 
 # @bot.command('roll')
 # async def roll():
