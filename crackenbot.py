f29e7e2265853bed8582a81a1f62647a2c3a163d
elissahuang <elissa.huang1@gmail.com>
2021-02-10 13:25:40 -0500

    carrot now requires a question to be asked

--- crackenbot.py
+++ crackenbot.py
@@ -252,28 +252,31 @@ async def broccoli(ctx):
 
 @bot.command('carrot', help="To do or not to do, that is the question.")
 async def carrot(ctx, *args):
-    responses = [
-        'As I see it, yes.', 
-        'Ask again later.', 
-        'Better not tell you now.', 
-        'Cannot predict now.', 
-        'Concentrate and ask again.',
-        "Don't count on it.",
-        'It is certain.',
-        'It is decidedly so.',
-        'Most likely.',
-        'My reply is no.',
-        'My sources say no.',
-        'Outlook not so good.',
-        'Outlook good.',
-        'Reply hazy, try again.',
-        'Signs point to yes.',
-        'Very doubtful.',
-        'Without a doubt.',
-        'Yes.',
-        'Yes - definitely.',
-        'You may rely on it.'
-    ]
-    await ctx.send('```' + random.choice(responses) + '```')
+    if len(args) == 0:
+        await ctx.send('```Ask me a question first.```')
+    else:
+        responses = [
+            'As I see it, yes.', 
+            'Ask again later.', 
+            'Better not tell you now.', 
+            'Cannot predict now.', 
+            'Concentrate and ask again.',
+            "Don't count on it.",
+            'It is certain.',
+            'It is decidedly so.',
+            'Most likely.',
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good.',
+            'Outlook good.',
+            'Reply hazy, try again.',
+            'Signs point to yes.',
+            'Very doubtful.',
+            'Without a doubt.',
+            'Yes.',
+            'Yes - definitely.',
+            'You may rely on it.'
+        ]
+        await ctx.send('```' + random.choice(responses) + '```')
 
 bot.run(TOKEN)
